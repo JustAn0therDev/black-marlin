566d1e98df3432674d103eee904c47e3d82d6415
Ruan Montelo <ruanmontelo@gmail.com>
2021-06-18 16:10:37 -0300

    Updating unittests.

--- unittests/black_marlin_tests.cpp
+++ unittests/black_marlin_tests.cpp
@@ -2,25 +2,32 @@
 #include "catch2.hpp"
 #include "../black_marlin.hpp"
 
-auto blackMarlin = BlackMarlin();
+auto black_marlin = BlackMarlin();
 
 TEST_CASE("Black Marlin Get and Set Tests", "[Get and Set]")
 {
 	std::cout << "Running Get and Set" << std::endl;
 
-	std::string sampleKey = "get and set test";
-    std::string anotherSampleKey = "get and set test two";
+	std::string sample_key = "get and set test";
 
-	std::string& sampleReference = sampleKey;
+	black_marlin.Set(sample_key, &sample_key);
 
-	blackMarlin.Set(sampleKey, new std::string(sampleKey));
-	REQUIRE(blackMarlin.Count() == 1);
-
-    blackMarlin.Set(anotherSampleKey, new std::string(anotherSampleKey));
-
-	std::string value = blackMarlin.Get(sampleKey);
+	std::string value = black_marlin.Get(sample_key);
 	REQUIRE(value != "");
 	REQUIRE(value == "get and set test");
+}
+
+TEST_CASE("Overwrite tests", "[Overwrite]")
+{
+	std::cout << "Running Overwrite" << std::endl;
+
+	std::string sample_key = "get and set test";
+	std::string val = "get and set test two";
+
+	black_marlin.Overwrite(sample_key, &val);
+
+	std::string value = black_marlin.Get(sample_key);
+	REQUIRE(value == "get and set test two");
 }
 
 TEST_CASE("Exists Tests", "[Exists]")
@@ -28,8 +35,7 @@ TEST_CASE("Exists Tests", "[Exists]")
 	std::cout << "Running Exists" << std::endl;
 
     std::string key = "get and set test";
-	std::string& ref = key;
-	REQUIRE(blackMarlin.Exists(key));
+	REQUIRE(black_marlin.Exists(key));
 }
 
 TEST_CASE("Delete Tests", "[Delete]")
@@ -37,23 +43,21 @@ TEST_CASE("Delete Tests", "[Delete]")
 	std::cout << "Running Delete" << std::endl;
 
     std::string key = "get and set test";
-	std::string& ref = key;
-    blackMarlin.Delete(ref);
+    black_marlin.Delete(key);
 
-    REQUIRE(blackMarlin.Count() == 1);
+    REQUIRE(black_marlin.Count() == 0);
 }
 
 TEST_CASE("Count Test", "[Count]")
 {
 	std::cout << "Running Count" << std::endl;
-
-	REQUIRE(blackMarlin.Count() == 1);
+	REQUIRE(black_marlin.Count() == 0);
 }
 
 TEST_CASE("Flush Test", "[Flush]")
 {
 	std::cout << "Running Flush" << std::endl;
 
-    blackMarlin.Flush();
-    REQUIRE(blackMarlin.Count() == 0);
+    black_marlin.Flush();
+    REQUIRE(black_marlin.Count() == 0);
 }
