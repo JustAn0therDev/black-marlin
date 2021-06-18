c5dc6b1644727d6a8de9f1f586493699f8a50cdf
Ruan Montelo <ruanmontelo@gmail.com>
2021-06-18 12:04:11 -0300

    Refactoring the code and fixing memory leak where an std::string pointer was not cleared when a key was overwritten.

--- http_request_handler.cpp
+++ http_request_handler.cpp
@@ -3,93 +3,80 @@
 #include "black_marlin.hpp"
 #include "status_codes.hpp"
 
-void HttpRequestHandler::HandleGet(BlackMarlin*& p_blackMarlin, const httplib::Request& p_req, httplib::Response& p_res) {
+void HttpRequestHandler::HandleGet(BlackMarlin*& p_black_marlin, const httplib::Request& p_req, httplib::Response& p_res) {
 	std::string value;
 
-	if (p_req.has_param("key")) {
-		value = p_blackMarlin->Get(p_req.get_param_value("key"));
-
-		if (std::empty(value)) {
-			p_res.status = (int)StatusCodes::kNoContent;
-		}
-	}
-	else {
+	if (!p_req.has_param("key")) {
 		p_res.status = (int)StatusCodes::kBadRequest;
+		return;
 	}
 
-	p_res.set_content(value, "*/*");
+	value = p_black_marlin->Get(p_req.get_param_value("key"));
+
+	if (std::empty(value)) {
+		p_res.status = (int)StatusCodes::kNoContent;
+		return;
+	}
+
+	p_res.set_content(value, this->m_content_type);
 }
 
-void HttpRequestHandler::HandlePost(BlackMarlin*& p_blackMarlin, const httplib::Request& p_req, httplib::Response& p_res) {
-	if (p_req.has_param("key")) {
-		std::string key = p_req.get_param_value("key");
-		std::string* body = new std::string(p_req.body);
-		std::string& keyRef = key;
-
-		p_blackMarlin->Set(keyRef, body);
-
-		p_res.status = (int)StatusCodes::kCreated;
-	}
-	else {
+void HttpRequestHandler::HandlePost(BlackMarlin*& p_black_marlin, const httplib::Request& p_req, httplib::Response& p_res) {
+	if (!p_req.has_param("key") || p_req.body == "") {
 		p_res.status = (int)StatusCodes::kBadRequest;
+		return;
 	}
 
-	p_res.set_content("", "*/*");
+	std::string* req_body_ptr = new std::string(p_req.body);
+	p_black_marlin->Set(p_req.get_param_value("key"), req_body_ptr);
+	p_res.status = (int)StatusCodes::kCreated;
 }
 
-void HttpRequestHandler::HandlePutAndPatch(BlackMarlin*& p_blackMarlin, const httplib::Request& p_req, httplib::Response& p_res) {
-	if (p_req.has_param("key")) {
-		std::string key = p_req.get_param_value("key");
-		std::string* body = new std::string(p_req.body);
-		std::string& keyRef = key;
-
-		p_blackMarlin->Set(keyRef, body);
-
-		p_res.status = (int)StatusCodes::kCreated;
-	}
-	else {
+void HttpRequestHandler::HandlePutAndPatch(BlackMarlin*& p_black_marlin, const httplib::Request& p_req, httplib::Response& p_res) {
+	if (!p_req.has_param("key") || p_req.body == "") {
 		p_res.status = (int)StatusCodes::kBadRequest;
+		return;
 	}
 
-	p_res.set_content("", "*/*");
+	std::string key = p_req.get_param_value("key");
+	std::string* body = new std::string(p_req.body);
+	std::string& keyRef = key;
+
+	if (!p_black_marlin->Exists(keyRef)) {
+		p_res.status = (int)StatusCodes::kBadRequest;
+		return;
+	}
+
+	p_black_marlin->Overwrite(keyRef, body);
+
+	p_res.set_content("", this->m_content_type);
 }
 
-void HttpRequestHandler::HandleDelete(BlackMarlin*& p_blackMarlin, const httplib::Request& p_req, httplib::Response& p_res) {
-	if (p_req.has_param("key")) {
-		std::string& keyRef = p_req.get_param_value("key");
-
-		p_blackMarlin->Delete(keyRef);
-	}
-	else {
+void HttpRequestHandler::HandleDelete(BlackMarlin*& p_black_marlin, const httplib::Request& p_req, httplib::Response& p_res) {
+	if (!p_req.has_param("key")) {
 		p_res.status = (int)StatusCodes::kBadRequest;
+		return;
 	}
 
-	p_res.set_content("", "*/*");
+	p_black_marlin->Delete(p_req.get_param_value("key"));
 }
 
-void HttpRequestHandler::HandleDeleteFlush(BlackMarlin*& p_blackMarlin, httplib::Response& p_res) {
-	p_blackMarlin->Flush();
+void HttpRequestHandler::HandleDeleteFlush(BlackMarlin*& p_black_marlin, httplib::Response& p_res) {
+	p_black_marlin->Flush();
 	p_res.status = (int)StatusCodes::kNoContent;
-	p_res.set_content("", "*/*");
 }
 
-void HttpRequestHandler::HandleGetExists(BlackMarlin*& p_blackMarlin, const httplib::Request& p_req, httplib::Response& p_res) {
-	if (p_req.has_param("key")) {
-		std::string& keyRef = p_req.get_param_value("key");
-
-		bool exists = p_blackMarlin->Exists(keyRef);
-
-		if (!exists) {
-			p_res.status = (int)StatusCodes::kNoContent;
-		}
-	}
-	else {
+void HttpRequestHandler::HandleGetExists(BlackMarlin*& p_black_marlin, const httplib::Request& p_req, httplib::Response& p_res) {
+	if (!p_req.has_param("key")) {
 		p_res.status = (int)StatusCodes::kBadRequest;
+		return;
 	}
 
-	p_res.set_content("", "*/*");
+	if (!p_black_marlin->Exists(p_req.get_param_value("key"))) {
+		p_res.status = (int)StatusCodes::kNoContent;
+	}
 }
 
-void HttpRequestHandler::HandleGetCount(BlackMarlin*& p_blackMarlin, httplib::Response& p_res) {
-	p_res.set_content(std::to_string(p_blackMarlin->Count()), "*/*");
+void HttpRequestHandler::HandleGetCount(BlackMarlin*& p_black_marlin, httplib::Response& p_res) {
+	p_res.set_content(std::to_string(p_black_marlin->Count()), this->m_content_type);
 }
--- http_request_handler.hpp
+++ http_request_handler.hpp
@@ -5,11 +5,21 @@
 
 class HttpRequestHandler {
 public:
+    // Handles a Get request.
     void HandleGet(BlackMarlin*& p_blackMarlin, const httplib::Request& p_req, httplib::Response& p_res);
+    // Handles a Post request.
     void HandlePost(BlackMarlin*& p_blackMarlin, const httplib::Request& p_req, httplib::Response& p_res);
+    // Handles Put and Patch requests.
     void HandlePutAndPatch(BlackMarlin*& p_blackMarlin, const httplib::Request& p_req, httplib::Response& p_res);
+    // Handles a Delete request.
     void HandleDelete(BlackMarlin*& p_blackMarlin, const httplib::Request& p_req, httplib::Response& p_res);
+    // Handles a Get request in the "/exists" route.
     void HandleGetExists(BlackMarlin*& p_blackMarlin, const httplib::Request& p_req, httplib::Response& p_res);
+    // Handles a Get request in the "/count" route.
     void HandleGetCount(BlackMarlin*& p_blackMarlin, httplib::Response& p_res);
+    // Handles a Delete request in the "/flush" route.
     void HandleDeleteFlush(BlackMarlin*& p_blackMarlin, httplib::Response& p_res);
+private:
+    // Content-Type header value if needed.
+    const char* m_content_type = "*/*; charset=utf-8";
 };
