8bce97dda2fbd2983a758a25f8be82a5ae4efd8a
Ruan Montelo <ruanmontelo@gmail.com>
2021-06-20 20:52:27 -0300

    Removing files not required to compile or edit the code

--- out/build/x64-Debug (default)/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/highl/source/repos/black_marlin
-# Build directory: C:/Users/highl/source/repos/black_marlin/out/build/x64-Debug (default)
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
