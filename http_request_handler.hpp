a8e2e6c12df9e16e24bc35a7170b75c7c61cf914
Ruan Montelo <ruanmontelo@gmail.com>
2021-06-27 17:20:28 -0300

    Fixing indentation and adding "util.hpp" to CMakeLists.txt

--- http_request_handler.hpp
+++ http_request_handler.hpp
@@ -5,23 +5,23 @@
 #define HTTPREQUESTHANDLERCPP
 class HttpRequestHandler {
 public:
-    // Handles a Get request.
-    void HandleGet(const BlackMarlin& p_blackMarlin, const httplib::Request& p_req, httplib::Response& p_res) const;
-    // Handles a Post request.
-    void HandlePost(BlackMarlin& p_blackMarlin, const httplib::Request& p_req, httplib::Response& p_res);
-    // Handles Put and Patch requests.
-    void HandlePutAndPatch(BlackMarlin& p_blackMarlin, const httplib::Request& p_req, httplib::Response& p_res);
-    // Handles a Delete request.
-    void HandleDelete(BlackMarlin& p_blackMarlin, const httplib::Request& p_req, httplib::Response& p_res);
-    // Handles a Delete request in the "/flush" route.
-    void HandleDeleteFlush(BlackMarlin& p_blackMarlin, httplib::Response& p_res);
-    // Handles a Get request in the "/exists" route.
-    void HandleGetExists(const BlackMarlin& p_blackMarlin, const httplib::Request& p_req, httplib::Response& p_res) const;
-    // Handles a Get request in the "/count" route.
-    void HandleGetCount(const BlackMarlin& p_blackMarlin, httplib::Response& p_res) const;
+	// Handles a Get request.
+	void HandleGet(const BlackMarlin& p_blackMarlin, const httplib::Request& p_req, httplib::Response& p_res) const;
+	// Handles a Post request.
+	void HandlePost(BlackMarlin& p_blackMarlin, const httplib::Request& p_req, httplib::Response& p_res);
+	// Handles Put and Patch requests.
+	void HandlePutAndPatch(BlackMarlin& p_blackMarlin, const httplib::Request& p_req, httplib::Response& p_res);
+	// Handles a Delete request.
+	void HandleDelete(BlackMarlin& p_blackMarlin, const httplib::Request& p_req, httplib::Response& p_res);
+	// Handles a Delete request in the "/flush" route.
+	void HandleDeleteFlush(BlackMarlin& p_blackMarlin, httplib::Response& p_res);
+	// Handles a Get request in the "/exists" route.
+	void HandleGetExists(const BlackMarlin& p_blackMarlin, const httplib::Request& p_req, httplib::Response& p_res) const;
+	// Handles a Get request in the "/count" route.
+	void HandleGetCount(const BlackMarlin& p_blackMarlin, httplib::Response& p_res) const;
 private:
-    // Content-Type header value if needed.
-    const char* m_content_type = "*/*; charset=utf-8";
-    bool IsValidSecondsParam(std::string p_seconds_param);
+	// Content-Type header value if needed.
+	const char* m_content_type = "*/*; charset=utf-8";
+	bool IsValidSecondsParam(std::string p_seconds_param);
 };
 #endif
