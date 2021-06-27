23920a080218262c2ba76889e83dce6b4065cb03
Ruan Montelo <ruanmontelo@gmail.com>
2021-06-27 17:48:31 -0300

    Removing valgrind execution from "run_with_gcc.sh"

--- run_with_gcc.sh
+++ run_with_gcc.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 g++ -pthread -std=c++17 black_marlin.cpp black_marlin.hpp httplib.h main.cpp http_request_handler.cpp http_request_handler.hpp util.hpp util.cpp -o black_marlin
-valgrind --leak-check=full ./black_marlin
+./black_marlin
