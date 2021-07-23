08096041cf25b1c281b86b3c2ac14f566844c287
Ruan Montelo <ruanmontelo@gmail.com>
2021-07-23 00:52:34 -0300

    Working file read for Windows and integrating the loaded headers to all responses

--- main.cpp
+++ main.cpp
@@ -6,41 +6,50 @@
 #include "black_marlin.hpp"
 #include "http_request_handler.hpp"
 
-int main() {
+int main() 
+{
 	httplib::Server server;
 
 	auto black_marlin = BlackMarlin();
-	HttpRequestHandler http_request_handler = HttpRequestHandler();
+	auto http_request_handler = HttpRequestHandler();
 
 	server.Get("/", [&black_marlin, &http_request_handler](const httplib::Request& req, httplib::Response& res) {
+		http_request_handler.SetResponseHeadersFromConfig(res);
 		http_request_handler.HandleGet(black_marlin, req, res);
 	});
 
 	server.Post("/", [&black_marlin, &http_request_handler](const httplib::Request& req, httplib::Response& res) {
+		http_request_handler.SetResponseHeadersFromConfig(res);
 		http_request_handler.HandlePost(black_marlin, req, res);
 	});
 
 	server.Put("/", [&black_marlin, &http_request_handler](const httplib::Request& req, httplib::Response& res) {
+		http_request_handler.SetResponseHeadersFromConfig(res);
 		http_request_handler.HandlePutAndPatch(black_marlin, req, res);
 	});
 
 	server.Patch("/", [&black_marlin, &http_request_handler](const httplib::Request& req, httplib::Response& res) {
+		http_request_handler.SetResponseHeadersFromConfig(res);
 		http_request_handler.HandlePutAndPatch(black_marlin, req, res);
 	});
 
 	server.Delete("/", [&black_marlin, &http_request_handler](const httplib::Request& req, httplib::Response& res) {
+		http_request_handler.SetResponseHeadersFromConfig(res);
 		http_request_handler.HandleDelete(black_marlin, req, res);
 	});
 
 	server.Delete("/flush", [&black_marlin, &http_request_handler](const httplib::Request&, httplib::Response& res) {
+		http_request_handler.SetResponseHeadersFromConfig(res);
 		http_request_handler.HandleDeleteFlush(black_marlin, res);
 	});
 
 	server.Get("/exists", [&black_marlin, &http_request_handler](const httplib::Request& req, httplib::Response& res) {
+		http_request_handler.SetResponseHeadersFromConfig(res);
 		http_request_handler.HandleGetExists(black_marlin, req, res);
 	});
 
 	server.Get("/count", [&black_marlin, &http_request_handler](const httplib::Request&, httplib::Response& res) {
+		http_request_handler.SetResponseHeadersFromConfig(res);
 		http_request_handler.HandleGetCount(black_marlin, res);
 	});
 
---
+++ server_configs.hpp
@@ -0,0 +1,35 @@
+#include <unordered_map>
+#include <string>
+#include <sstream>
+#include <utility>
+
+#if _WIN32
+#define PATH_DELIMETER '\\'
+#else 
+#define PATH_DELIMETER '\'
+#endif
+
+#ifndef SERVERCONFIGSCPP
+#define SERVERCONFIGSCPP
+class ServerConfigs 
+{
+public:
+	ServerConfigs();
+	// Loads the internal hashmap with the configs file's content.
+	void LoadHeadersFromConfigFile();
+	// Returns a const reference to the hashmap
+	const std::unordered_map<std::string, std::string>& GetConfigs() const;
+private:
+	// This object's internal hashmap.
+	std::unordered_map<std::string, std::string> m_configs;
+	// The current operating system's path delimeter.
+	// The header configs file name.
+	std::string m_filename;
+	// Gets the correct path for the config file.
+	const std::string GetThisExecutingBinaryFullPath();
+	// Returns the object's internal hashmap for reading only.
+	const std::unordered_map<std::string, std::string> GetConfigsFromFile() const;
+	// Returns the header information (being it key and value) from the config file's current line.
+	const std::pair<std::string, std::string> GetHeaderPairFromConfigFile(std::istringstream& p_iss, char& p_split_by);
+};
+#endif /* SERVERCONFIGSCPP */
