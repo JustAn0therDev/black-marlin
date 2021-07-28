56e725fce240cd8368e5d46ca721730f5aaa1220
Ruan Montelo <ruanmontelo@gmail.com>
2021-07-28 10:13:06 -0300

    Fixing port argument bug by changing the port validation variable's data type allowing it to exceed the USHRT_MAX macro

--- ArgParser/arg_parser.cpp
+++ ArgParser/arg_parser.cpp
@@ -4,7 +4,7 @@
 
 short ArgParser::GetPortFromArg(const char* arg_value) noexcept
 {
-	short port = 0;
+	unsigned int port = 0;
 	std::regex only_numbers_pattern("[0-9]+");
 
 	if (regex_match(arg_value, only_numbers_pattern))
@@ -15,7 +15,7 @@ short ArgParser::GetPortFromArg(const char* arg_value) noexcept
 		Util::Panic("Expected only numbers for port argument. Got: " + std::string(arg_value) + "\n");
 
 	if (port > USHRT_MAX)
-		Util::Panic("The port argument exceeds the range of valid ports.");
+		Util::Panic("The port argument value exceeds the range of valid ports.");
 
 	return port;
 }
