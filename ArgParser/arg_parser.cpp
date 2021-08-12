c53720046f6521f4dbb56d59f05404e6b3d5c8a9
Ruan Montelo <48066526+JustAn0therDev@users.noreply.github.com>
2021-08-12 18:01:20 -0300

    Feature/more unittests 20210812 (#16)
    * Adding test for ArgParser and changing the place where the port argument was being validated
    
    * Testing the rest of the modules that can't be tested outside of the program with integration tests.

--- ArgParser/arg_parser.cpp
+++ ArgParser/arg_parser.cpp
@@ -6,6 +6,12 @@
 
 long ArgParser::GetPortFromArg(const char* arg_value) noexcept
 {
+    const std::regex only_numbers("^[0-9]+$");
+
+    if (!std::regex_match(arg_value, only_numbers)) {
+        return 0;
+    }
+
     char* buf;
 	long port = std::strtol(arg_value, &buf, 10);
 
--- unittests/arg_parser_tests.cpp
+++ unittests/arg_parser_tests.cpp
@@ -1,7 +1,21 @@
+#include <iostream>
 #include "../vendor/catch2.hpp"
 #include "../ArgParser/arg_parser.hpp"
 
 TEST_CASE("Should parse port argument correctly.", "[GetPortFromArg]") 
 {
+    std::cout << "Running GetPortFromArg - Should parse port argument correctly.\n";
     REQUIRE(ArgParser::GetPortFromArg("1243") == 1243);
+}
+
+TEST_CASE("Should try to parse an invalid port argument and maybe return 0", "[GetPortFromArg]") 
+{
+    std::cout << "Running GetPortFromArg - Should try to parse an invalid port argument and return 0.\n";
+    REQUIRE(ArgParser::GetPortFromArg("-2") == 0);
+}
+
+TEST_CASE("Should try to parse an invalid port argument and return 0", "[GetPortFromArg]") 
+{
+    std::cout << "Running GetPortFromArg - Should try to parse an invalid port argument and return 0.\n";
+    REQUIRE(ArgParser::GetPortFromArg("utheasuaoeh") == 0);
 }
