3e4b1dca619a9d246a9a504c735b310e2172aee5
Ruan <ruanmontelo@gmail.com>
2021-10-03 13:48:14 -0300

    Changing panic not to log error, refactoring Logger module usage with C strings and default sizes for the log messages

--- Logger/logger.hpp
+++ Logger/logger.hpp
@@ -10,8 +10,12 @@
 constexpr char LOG_FILENAME[] = "bm_logs.txt";
 // The log message delimeter. This separates the normal message with the time the error occurred.
 constexpr char LOG_MESSAGE_DELIMETER[] = " - ";
-// The time message buffer's size. Since we don't always know the size of a key or value, this value will vary in future versions.
-constexpr int TIME_BUFFER_SIZE = 100;
+// The size of the log message.
+// A key can easily exceed more than 200 characters considering it will almost always be some kind of big string or JSON structure.
+constexpr int LOG_MESSAGE_SIZE = 600;
+// The time message buffer's size. The size of the formatted time string including the size of the log message.
+constexpr int TIME_STRING_SIZE = LOG_MESSAGE_SIZE + 20;
+
 
 class Logger
 {
@@ -19,7 +23,7 @@ class Logger
         Logger();
         ~Logger();
         // Logs the message to the log file.
-        void Log(const std::string& p_message) noexcept;
+        void Log(const char* p_message) noexcept;
     private:
         // The "LOG_FILENAME"'s full path to allow writing.
         std::string m_full_filepath{};
