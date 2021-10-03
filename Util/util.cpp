245a34c6fb203a8855d00f5ae7e6baeb1f044034
Ruan <ruanmontelo@gmail.com>
2021-10-03 13:52:13 -0300

    Changing the Panic function in the Util modules to receive a const std::string& and adding breakline char at the end of the message in the main function.

--- Util/util.cpp
+++ Util/util.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 #include "util.hpp"
 
-void Util::Panic(const char* p_panic_message) noexcept
+void Util::Panic(const std::string& p_panic_message) noexcept
 {
     std::cout << p_panic_message;
     exit(1);
